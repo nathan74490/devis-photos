cb302d2a1258de8a75f0d49aa0d1bfd27125ecbc
nathan74490 <na.boudet@formation-cci.fr>
2025-10-15 22:33:19 +0200

    feat: update database configuration and add new API routes for formats, tariffs, and support

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,19 +2,22 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { query } from "./db.js";
-import devisRoutes from "./routes/devis.js"; // 👈 import des routes
-
+import formatsRoutes from "./routes/formats.js"; // 👈 importe ta route
+import tariffsRoutes from "./routes/tariffs.js";
+import supportRoutes from "./routes/support.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes principales
-app.get("/", (req, res) => res.send("API Node + PostgreSQL en ligne 🚀"));
+// Route test pour vérifier que le serveur répond
+app.get("/", (req, res) => res.send("API Node connectée à PostgreSQL 🚀"));
 
-// Routes pour les devis
-app.use("/api/devis", devisRoutes); // 👈 toutes les routes de devis seront sous /api/devis/...
+// Utilisation de la route
+app.use("/api/formats", formatsRoutes); // 👈 toutes les requêtes /api/formats passent par ton fichier formats.js
+app.use("/api/tariffs", tariffsRoutes); // 👈 toutes les requêtes /api/tariffs passent par ton fichier tariffs.js
+app.use("/api/support", supportRoutes); // 👈 toutes les requêtes /api/support passent par ton fichier support.js
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`✅ Serveur en ligne sur http://localhost:${PORT}`));
