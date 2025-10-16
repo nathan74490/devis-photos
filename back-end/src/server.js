39cdcc475354f0a925d33f2a056bad04ba657a17
nathan74490 <na.boudet@formation-cci.fr>
2025-10-16 08:57:13 +0200

    refactor: clean up comments in server.js for better readability

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("API Node connectée à PostgreSQL 🚀"));
 
 // Utilisation de la route
-app.use("/api/formats", formatsRoutes); // 👈 toutes les requêtes /api/formats passent par ton fichier formats.js
-app.use("/api/tariffs", tariffsRoutes); // 👈 toutes les requêtes /api/tariffs passent par ton fichier tariffs.js
-app.use("/api/support", supportRoutes); // 👈 toutes les requêtes /api/support passent par ton fichier support.js
+app.use("/api/formats", formatsRoutes); 
+app.use("/api/tariffs", tariffsRoutes); 
+app.use("/api/support", supportRoutes); 
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`✅ Serveur en ligne sur http://localhost:${PORT}`));
